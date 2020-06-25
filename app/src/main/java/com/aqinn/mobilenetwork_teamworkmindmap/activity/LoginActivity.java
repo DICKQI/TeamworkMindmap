36de6e4efbce22d4626e07dda68b83d78c294629
kwuojr <>
2020-06-25 19:21:09 +0800

    添加个人中心布局，修改代码规范

--- app/src/main/java/com/aqinn/mobilenetwork_teamworkmindmap/activity/LoginActivity.java
+++ app/src/main/java/com/aqinn/mobilenetwork_teamworkmindmap/activity/LoginActivity.java
@@ -21,15 +21,15 @@ import java.util.regex.Pattern;
 public class LoginActivity extends AppCompatActivity {
 
     //按钮设置
-    private EditText user_edit;
-    private EditText passwd_edit;
-    private EditText passwd2_edit;
-    private Button login_btn;
-    private Button register_btn;
-    private Button back_btn;
-    private ImageView img3view;
-    private ImageView img4view;
-    private TextView text4view;
+    private EditText edtTxt_username;
+    private EditText edtTxt_passwd;
+    private EditText edtTxt_passwd2;
+    private Button bt_login;
+    private Button bt_register;
+    private Button bt_back;
+    private ImageView iv_passwd2;
+    private ImageView iv_verify;
+    private TextView tv_verifyText;
     private String username;
     private String password;
     private String password2;
@@ -45,22 +45,22 @@ public class LoginActivity extends AppCompatActivity {
 //        requestWindowFeature(Window.FEATURE_NO_TITLE);
 //        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
-        user_edit = findViewById(R.id.username_edit);
-        passwd_edit = findViewById(R.id.password_edit);
-        passwd2_edit = findViewById(R.id.password2_edit);
-        login_btn = findViewById(R.id.login_btn);
-        back_btn = findViewById(R.id.login_back_btn);
-        register_btn = findViewById(R.id.register_btn);
-        img3view = findViewById(R.id.imgView3);
-        img4view = findViewById(R.id.imgView4);
-        text4view = findViewById(R.id.textviewverify);
+        edtTxt_username = findViewById(R.id.username_edit);
+        edtTxt_passwd = findViewById(R.id.password_edit);
+        edtTxt_passwd2 = findViewById(R.id.password2_edit);
+        bt_login = findViewById(R.id.login_btn);
+        bt_back = findViewById(R.id.login_back_btn);
+        bt_register = findViewById(R.id.register_btn);
+        iv_passwd2 = findViewById(R.id.login_passwd2_img);
+        iv_verify = findViewById(R.id.login_verify_img);
+        tv_verifyText = findViewById(R.id.textviewverify);
 
         //登录按钮
-        login_btn.setOnClickListener(new View.OnClickListener() {
+        bt_login.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View view) {
-                username = user_edit.getText().toString().trim();
-                password = passwd_edit.getText().toString().trim();
+                username = edtTxt_username.getText().toString().trim();
+                password = edtTxt_passwd.getText().toString().trim();
                 loginin_socket();
                 Intent intent = new Intent();
                 intent.setClass(LoginActivity.this, IndexActivity.class);
@@ -71,35 +71,35 @@ public class LoginActivity extends AppCompatActivity {
         });
 
         //注册按钮
-        register_btn.setOnClickListener(new View.OnClickListener() {
+        bt_register.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View view) {
 
-                login_btn.setVisibility(View.INVISIBLE);
-                passwd2_edit.setVisibility(View.VISIBLE);
-                img3view.setVisibility(View.VISIBLE);
-                img4view.setVisibility(View.VISIBLE);
-                text4view.setVisibility(View.VISIBLE);
-                back_btn.setVisibility(View.VISIBLE);
-                register_btn.setText("立即注册");
+                bt_login.setVisibility(View.INVISIBLE);
+                edtTxt_passwd2.setVisibility(View.VISIBLE);
+                iv_passwd2.setVisibility(View.VISIBLE);
+                iv_verify.setVisibility(View.VISIBLE);
+                tv_verifyText.setVisibility(View.VISIBLE);
+                bt_back.setVisibility(View.VISIBLE);
+                bt_register.setText("立即注册");
                 if (isRegister) {
                     if (isVerify != true) {
-                        text4view.setText("该用户名已被注册，请重新输入");
-                        text4view.setBackgroundColor(Color.RED);
-                        img4view.setImageResource(R.mipmap.ic_unverified);
+                        tv_verifyText.setText("该用户名已被注册，请重新输入");
+                        tv_verifyText.setBackgroundColor(Color.RED);
+                        iv_verify.setImageResource(R.mipmap.ic_unverified);
                     } else if (isCorrected != true) {
-                        text4view.setText("密码格式不正确");
-                        text4view.setBackgroundColor(Color.RED);
-                        img4view.setImageResource(R.mipmap.ic_unverified);
+                        tv_verifyText.setText("密码格式不正确");
+                        tv_verifyText.setBackgroundColor(Color.RED);
+                        iv_verify.setImageResource(R.mipmap.ic_unverified);
                     } else if (isEqual != true) {
-                        text4view.setText("两次密码不一致");
-                        text4view.setBackgroundColor(Color.RED);
-                        img4view.setImageResource(R.mipmap.ic_unverified);
+                        tv_verifyText.setText("两次密码不一致");
+                        tv_verifyText.setBackgroundColor(Color.RED);
+                        iv_verify.setImageResource(R.mipmap.ic_unverified);
                     } else {
 
-                        username = user_edit.getText().toString().trim();
-                        password = passwd_edit.getText().toString().trim();
-                        password2 = passwd2_edit.getText().toString().trim();
+                        username = edtTxt_username.getText().toString().trim();
+                        password = edtTxt_passwd.getText().toString().trim();
+                        password2 = edtTxt_passwd2.getText().toString().trim();
                         register_socket();
                     }
                 }
@@ -109,25 +109,25 @@ public class LoginActivity extends AppCompatActivity {
         });
 
         //返回按钮
-        back_btn.setOnClickListener(new View.OnClickListener() {
+        bt_back.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View view) {
                 isRegister = false;
-                login_btn.setVisibility(View.VISIBLE);
-                passwd2_edit.setVisibility(View.INVISIBLE);
-                img3view.setVisibility(View.INVISIBLE);
-                img4view.setVisibility(View.INVISIBLE);
-                text4view.setVisibility(View.INVISIBLE);
-                back_btn.setVisibility(View.INVISIBLE);
-                user_edit.setText("");
-                passwd_edit.setText("");
-                passwd2_edit.setText("");
+                bt_login.setVisibility(View.VISIBLE);
+                edtTxt_passwd2.setVisibility(View.INVISIBLE);
+                iv_passwd2.setVisibility(View.INVISIBLE);
+                iv_verify.setVisibility(View.INVISIBLE);
+                tv_verifyText.setVisibility(View.INVISIBLE);
+                bt_back.setVisibility(View.INVISIBLE);
+                edtTxt_username.setText("");
+                edtTxt_passwd.setText("");
+                edtTxt_passwd2.setText("");
             }
 
         });
 
         //用户名监听
-        user_edit.addTextChangedListener(new TextWatcher() {
+        edtTxt_username.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
@@ -136,16 +136,16 @@ public class LoginActivity extends AppCompatActivity {
             //判断用户名是否被占用
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                String user = user_edit.getText().toString().trim();
+                String user = edtTxt_username.getText().toString().trim();
                 //HttpHelper.get("");
                 if (isVerify != true) {
-                    img4view.setImageResource(R.mipmap.ic_unverified);
-                    text4view.setText("该用户名已被注册，请重新输入");
-                    text4view.setBackgroundColor(Color.RED);
+                    iv_verify.setImageResource(R.mipmap.ic_unverified);
+                    tv_verifyText.setText("该用户名已被注册，请重新输入");
+                    tv_verifyText.setBackgroundColor(Color.RED);
                 } else {
-                    text4view.setText("该用户名可用");
-                    text4view.setBackgroundColor(Color.GREEN);
-                    img4view.setImageResource(R.mipmap.ic_verified);
+                    tv_verifyText.setText("该用户名可用");
+                    tv_verifyText.setBackgroundColor(Color.GREEN);
+                    iv_verify.setImageResource(R.mipmap.ic_verified);
                 }
             }
 
@@ -156,7 +156,7 @@ public class LoginActivity extends AppCompatActivity {
         });
 
         //密码符合
-        passwd_edit.addTextChangedListener(new TextWatcher() {
+        edtTxt_passwd.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
@@ -164,22 +164,22 @@ public class LoginActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                String passwd = passwd_edit.getText().toString().trim();
+                String passwd = edtTxt_passwd.getText().toString().trim();
                 Pattern pattern = Pattern.compile("([a-z]|[A-Z])*");
                 Pattern pattern1 = Pattern.compile("[0-9]*");
                 Matcher matcher = pattern.matcher(passwd);
                 Matcher matcher1 = pattern1.matcher((passwd));
-                text4view.setText("请输入正确的密码（包含字母和数字,6位以上）");
-                text4view.setBackgroundColor(Color.RED);
-                img4view.setImageResource(R.mipmap.ic_unverified);
+                tv_verifyText.setText("请输入正确的密码（包含字母和数字,6位以上）");
+                tv_verifyText.setBackgroundColor(Color.RED);
+                iv_verify.setImageResource(R.mipmap.ic_unverified);
                 isCorrected = false;
                 if (matcher.find()) {
                     if (matcher1.find()) {
                         if (passwd.length() >= 6) {
                             isCorrected = true;
-                            img4view.setImageResource(R.mipmap.ic_verified);
-                            text4view.setText("该用密码可用");
-                            text4view.setBackgroundColor(Color.GREEN);
+                            iv_verify.setImageResource(R.mipmap.ic_verified);
+                            tv_verifyText.setText("该用密码可用");
+                            tv_verifyText.setBackgroundColor(Color.GREEN);
                         }
                     }
                 }
@@ -192,7 +192,7 @@ public class LoginActivity extends AppCompatActivity {
         });
 
         //密码确认
-        passwd2_edit.addTextChangedListener(new TextWatcher() {
+        edtTxt_passwd2.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
@@ -200,19 +200,19 @@ public class LoginActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                String passwd2 = passwd2_edit.getText().toString().trim();
-                String passwd = passwd_edit.getText().toString().trim();
+                String passwd2 = edtTxt_passwd2.getText().toString().trim();
+                String passwd = edtTxt_passwd.getText().toString().trim();
                 if (isCorrected) {
-                    text4view.setText("请输入相同的密码");
-                    text4view.setBackgroundColor(Color.RED);
-                    img4view.setImageResource(R.mipmap.ic_unverified);
+                    tv_verifyText.setText("请输入相同的密码");
+                    tv_verifyText.setBackgroundColor(Color.RED);
+                    iv_verify.setImageResource(R.mipmap.ic_unverified);
                     isEqual = false;
                     if (passwd.equals(passwd2)) {
                         if (isVerify) {
-                            img4view.setImageResource(R.mipmap.ic_verified);
-                            text4view.setText("该用户名密码可用");
+                            iv_verify.setImageResource(R.mipmap.ic_verified);
+                            tv_verifyText.setText("该用户名密码可用");
                             isEqual = true;
-                            text4view.setBackgroundColor(Color.GREEN);
+                            tv_verifyText.setBackgroundColor(Color.GREEN);
                         }
                     }
                 }
--- app/src/main/java/com/aqinn/mobilenetwork_teamworkmindmap/view/ui/fragment/MyFragment.java
+++ app/src/main/java/com/aqinn/mobilenetwork_teamworkmindmap/view/ui/fragment/MyFragment.java
@@ -1,9 +1,18 @@
 package com.aqinn.mobilenetwork_teamworkmindmap.view.ui.fragment;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,11 +20,28 @@ import androidx.fragment.app.Fragment;
 
 import com.aqinn.mobilenetwork_teamworkmindmap.R;
 
+import java.io.File;
+import java.io.IOException;
+
+import static android.app.Activity.RESULT_OK;
+
 /**
  * @author Aqinn
  * @date 2020/6/14 10:42 PM
  */
-public class MyFragment extends Fragment {
+public class MyFragment extends Fragment  implements View.OnClickListener{
+
+    private ImageView iv_userIcon;
+    private EditText edtTxt_nickname;
+    private EditText edtTxt_account;
+    private EditText edtTxt_passwd;
+    private EditText edtTxt_signature;
+    private EditText edtTxt_email;
+    private RadioGroup rdoGp_gender;
+    private RadioButton rdoBt_gender_man;
+    private RadioButton rdoBt_gender_female;
+    private RadioButton rdoBt_gender_private;
+    private File cropImageFile;
 
     public static MyFragment newInstance() {
         MyFragment fragment = new MyFragment();
@@ -30,7 +56,83 @@ public class MyFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // 加载布局文件
         View view = inflater.inflate(R.layout.fragment_my, container, false);
+        initAllView(view);
         return view;
     }
 
+    private void initAllView(View v) {
+        iv_userIcon = v.findViewById(R.id.my_picture);
+        edtTxt_account = v.findViewById(R.id.my_account);
+        edtTxt_nickname = v.findViewById(R.id.my_name);
+        edtTxt_passwd = v.findViewById(R.id.my_password);
+        edtTxt_signature = v.findViewById(R.id.my_signature);
+        edtTxt_email = v.findViewById(R.id.my_email);
+        rdoGp_gender = v.findViewById(R.id.my_rdoGp);
+        rdoBt_gender_man = v.findViewById(R.id.my_rdo_genderMan);
+        rdoBt_gender_female = v.findViewById(R.id.my_rdo_genderFemale);
+        rdoBt_gender_private = v.findViewById(R.id.my_rdo_genderPrivate);
+
+        rdoGp_gender.setOnCheckedChangeListener(radioGrouplisten);
+        iv_userIcon.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.my_picture:
+                try {
+                    //选择照片的时候也一样，我们用Action为Intent.ACTION_GET_CONTENT，
+                    //有些人使用其他的Action但我发现在有些机子中会出问题，所以优先选择这个
+                    Intent intent = new Intent();
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    startActivityForResult(intent, 2);
+                } catch (ActivityNotFoundException e) {
+
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case 2:
+                    iv_userIcon.setImageURI(data.getData());
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+
+
+
+    //监听Radio
+    private RadioGroup.OnCheckedChangeListener radioGrouplisten = new RadioGroup.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+            int id = group.getCheckedRadioButtonId();
+            switch (group.getCheckedRadioButtonId()) {
+                case R.id.my_rdo_genderMan:
+//                    orientation = Orientation.landscape;
+//                    Log.i("orientation",orientation.toString());
+                    //Toast.makeText(PrintSettingActivity.this, orientation.toString(), Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.my_rdo_genderFemale:
+//                    orientation = Orientation.Portrait;
+//                    Log.i("orientation",orientation.toString());
+                    //Toast.makeText(PrintSettingActivity.this, orientation.toString(), Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.my_rdo_genderPrivate:
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 }
