a309f4b044631e89b99ac8766e729831b57559e0
kwuojr <>
2020-06-28 21:52:37 +0800

    添加restful地址返回

--- app/src/main/java/com/aqinn/mobilenetwork_teamworkmindmap/config/PublicConfig.java
+++ app/src/main/java/com/aqinn/mobilenetwork_teamworkmindmap/config/PublicConfig.java
@@ -13,12 +13,121 @@ public class PublicConfig {
     public static final String CONTENT_LOCATION = "twmm_content/";
     public static final String CONFIG_FILE = "conf.txt";
 
-    public static final String URL_REGISTER = "http://49.234.71.210/user/register/";
-    public static final String URL_LOGIN = "http://49.234.71.210/user/";
-    public static final String URL_LOGOUT = "http://49.234.71.210/user/";
-    public static final String URL_VERIFY_LOGIN = "http://49.234.71.210/user/";
-    public static final String URL_SHARE_MAP = "http://49.234.71.210/map/new/";
-    public static final String URL_SHARE_MAP_AGAIN = "http://49.234.71.210/map/";
+    /**
+     * 加入导图协作
+     *
+     * @param shareId
+     * @return
+     */
+    public static final String url_post_joinTeamWorkMindmap(final Long shareId) {
+        return "http://49.234.71.210/map/join/" + shareId + "/";
+    }
+
+    /**
+     * 再次开启共享同步导图
+     *
+     * @param shareId
+     * @return
+     */
+    public static final String url_put_shareOnAgain(final Long shareId) {
+        return "http://49.234.71.210/map/" + shareId + "/";
+    }
+
+    /**
+     * 获取导图列表
+     *
+     * @return
+     */
+    public static final String url_get_getMindmapList() {
+        return "http://49.234.71.210/map/list/";
+    }
+
+    /**
+     * 删除指定节点及其子节点
+     *
+     * @param shareId
+     * @param nodeId
+     * @return
+     */
+    public static final String url_delete_deleteNodeAndSubNode(final Long shareId, final Long nodeId) {
+        return "http://49.234.71.210/map/" + shareId + "/mod/" + nodeId + "/";
+    }
+
+
+    /**
+     * 修改节点信息
+     *
+     * @param shareId
+     * @param nodeId
+     * @return
+     */
+    public static final String url_put_editNode(final Long shareId, final Long nodeId) {
+        return "http://49.234.71.210/map/" + shareId + "/mod/" + nodeId + "/";
+    }
+
+    /**
+     * 新增节点
+     *
+     * @param shareId
+     * @param parentNodeId
+     * @return
+     */
+    public static final String url_post_addNode(final Long shareId, final Long parentNodeId) {
+        return "http://49.234.71.210/map/" + shareId + "/add/" + parentNodeId + "/";
+    }
+
+    /**
+     * 获取在线导图详情
+     *
+     * @param shareId
+     * @return
+     */
+    public static final String url_get_getMindmapDetail(final Long shareId) {
+        return "http://49.234.71.210/map/" + shareId + "/";
+    }
+
+    /**
+     * 创建在线导图/本地首次开启共享
+     *
+     * @param shareId
+     * @return
+     */
+    public static final String url_post_firstShareOn(final Long shareId) {
+        return "http://49.234.71.210/map/" + shareId + "/";
+    }
+
+    /**
+     * 登出账户
+     * @return
+     */
+    public static final String url_delete_logout() {
+        return "http://49.234.71.210/user/";
+    }
+
+    /**
+     * 登录账户
+     * @return
+     */
+    public static final String url_post_login() {
+        return "http://49.234.71.210/user/";
+    }
+
+    /**
+     * 验证登录
+     * @return
+     */
+    public static final String url_get_verifyLogin() {
+        return "http://49.234.71.210/user/";
+    }
+
+    /**
+     * 注册账户
+     * @return
+     */
+    public static final String url_post_register() {
+        return "http://49.234.71.210/user/register";
+    }
+
 
 
 }
